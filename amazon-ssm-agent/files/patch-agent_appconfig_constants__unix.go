99f33c1730aca2356f44ab3a1a365066b5f438f2
Veniamin Begemotenko <begemoth3663@gmail.com>
2021-11-05 13:57:47 +0159

    Upd amazon-ssm-agent 3.1.338.0 -> 3.1.501.0, go 1.16.9 -> 1.16.10

--- amazon-ssm-agent/files/patch-agent_appconfig_constants__unix.go
+++ amazon-ssm-agent/files/patch-agent_appconfig_constants__unix.go
@@ -1,6 +1,6 @@
---- agent/appconfig/constants_unix.go.orig	2020-11-17 21:26:34 UTC
+--- agent/appconfig/constants_unix.go.orig	2021-11-04 22:42:28 UTC
 +++ agent/appconfig/constants_unix.go
-@@ -28,7 +28,7 @@ import (
+@@ -29,7 +29,7 @@ import (
  const (
  
  	// AgentExtensions specified the root folder for various kinds of downloaded content
@@ -9,7 +9,7 @@
  
  	// PackageRoot specifies the directory under which packages will be downloaded and installed
  	PackageRoot = AgentData + "packages"
-@@ -61,10 +61,10 @@ const (
+@@ -62,10 +62,10 @@ const (
  	DefaultDataStorePath = AgentData
  
  	// EC2ConfigDataStorePath represents the directory for storing ec2 config data
@@ -22,7 +22,7 @@
  
  	// UpdaterArtifactsRoot represents the directory for storing update related information
  	UpdaterArtifactsRoot = AgentData + "update/"
-@@ -112,9 +112,9 @@ const (
+@@ -113,9 +113,9 @@ const (
  var PowerShellPluginCommandName string
  
  // DefaultProgramFolder is the default folder for SSM
@@ -31,10 +31,10 @@
  
 -var defaultWorkerPath = "/usr/bin/"
 +var defaultWorkerPath = "/usr/local/sbin/"
+ var DefaultSSMAgentBinaryPath = defaultWorkerPath + "amazon-ssm-agent"
  var DefaultSSMAgentWorker = defaultWorkerPath + "ssm-agent-worker"
  var DefaultDocumentWorker = defaultWorkerPath + "ssm-document-worker"
- var DefaultSessionWorker = defaultWorkerPath + "ssm-session-worker"
-@@ -133,9 +133,9 @@ func init() {
+@@ -137,9 +137,9 @@ func init() {
  	/*
  	   Powershell command used to be poweshell in alpha versions, now it's pwsh in prod versions
  	*/
