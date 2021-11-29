55534813180ff2835df395511527a5ebcb7a6f9f
Veniamin Begemotenko <begemoth3663@gmail.com>
2021-11-29 17:20:27 +0159

    Upd driftctl 0.16.1 -> 0.17.0

--- driftctl/files/patch-pkg_cmd_driftctl.go
+++ driftctl/files/patch-pkg_cmd_driftctl.go
@@ -1,13 +1,13 @@
---- pkg/cmd/driftctl.go.orig	2021-10-30 20:38:48 UTC
+--- pkg/cmd/driftctl.go.orig	1979-11-29 21:00:00 UTC
 +++ pkg/cmd/driftctl.go
-@@ -62,8 +62,8 @@ func NewDriftctlCmd(build build.BuildInterface) *Drift
- 	cmd.SetUsageTemplate(usageTemplate)
+@@ -63,8 +63,8 @@ func NewDriftctlCmd(build build.BuildInterface) *Drift
  
  	cmd.PersistentFlags().BoolP("help", "h", false, "Display help for command")
--	cmd.PersistentFlags().BoolP("no-version-check", "", false, "Disable the version check")
--	cmd.PersistentFlags().BoolP("disable-telemetry", "", false, "Disable telemetry")
-+	cmd.PersistentFlags().BoolP("no-version-check", "", true, "Disable the version check")
-+	cmd.PersistentFlags().BoolP("disable-telemetry", "", true, "Disable telemetry")
+ 	if cmd.build.IsUsageReportingEnabled() {
+-		cmd.PersistentFlags().BoolP("no-version-check", "", false, "Disable the version check")
+-		cmd.PersistentFlags().BoolP("disable-telemetry", "", false, "Disable telemetry")
++		cmd.PersistentFlags().BoolP("no-version-check", "", true, "Disable the version check")
++		cmd.PersistentFlags().BoolP("disable-telemetry", "", true, "Disable telemetry")
+ 	}
  	cmd.PersistentFlags().BoolP("send-crash-report", "", false, "Enable error reporting. Crash data will be sent to us via Sentry.\nWARNING: may leak sensitive data (please read the documentation for more details)\nThis flag should be used only if an error occurs during execution")
  
- 	cmd.AddCommand(NewScanCmd(&pkg.ScanOptions{}))
