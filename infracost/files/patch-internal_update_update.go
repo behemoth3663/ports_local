e3d53709638e4b7a61b42929049a8522412ee202
Oleksii Milevskyi <begemoth3663@gmail.com>
2024-09-15 23:22:41 +0300

    Upd infracost 0.10.31 -> 0.10.39 (change from M2T_OFFLINE to GO_MODULE)

--- infracost/files/patch-internal_update_update.go
+++ infracost/files/patch-internal_update_update.go
@@ -1,18 +1,31 @@
---- internal/update/update.go.orig	2023-12-05 22:53:36 UTC
+--- internal/update/update.go.orig	1979-11-29 21:00:00 UTC
 +++ internal/update/update.go
-@@ -14,11 +14,8 @@ import (
- 	"time"
+@@ -1,24 +1,7 @@ import (
+ package update
  
- 	"github.com/pkg/errors"
--	"github.com/rs/zerolog/log"
+ import (
+-	"bytes"
+-	"encoding/json"
+-	"fmt"
+-	"io"
+-	"net/http"
+-	"os"
+-	"os/exec"
+-	"path/filepath"
+-	"runtime"
+-	"strings"
+-	"time"
+-
+-	"github.com/pkg/errors"
 -	"golang.org/x/mod/semver"
- 
+-
  	"github.com/infracost/infracost/internal/config"
+-	"github.com/infracost/infracost/internal/logging"
 -	"github.com/infracost/infracost/internal/version"
  )
  
  type Info struct {
-@@ -27,66 +24,7 @@ func CheckForUpdate(ctx *config.RunContext) (*Info, er
+@@ -27,193 +10,5 @@ func CheckForUpdate(ctx *config.RunContext) (*Info, er
  }
  
  func CheckForUpdate(ctx *config.RunContext) (*Info, error) {
@@ -23,7 +36,7 @@
 -	// Check cache for the latest version
 -	cachedLatestVersion, err := checkCachedLatestVersion(ctx)
 -	if err != nil {
--		log.Debug().Msgf("error getting cached latest version: %v", err)
+-		logging.Logger.Debug().Msgf("error getting cached latest version: %v", err)
 -	}
 -
 -	// Nothing to do if the current version is the latest cached version
@@ -34,7 +47,7 @@
 -	isBrew, err := isBrewInstall()
 -	if err != nil {
 -		// don't fail if we can't detect brew, just fallback to other update method
--		log.Debug().Msgf("error checking if executable was installed via brew: %v", err)
+-		logging.Logger.Debug().Msgf("error checking if executable was installed via brew: %v", err)
 -	}
 -
 -	var cmd string
@@ -64,7 +77,7 @@
 -	if latestVersion != cachedLatestVersion {
 -		err := setCachedLatestVersion(ctx, latestVersion)
 -		if err != nil {
--			log.Debug().Msgf("error saving cached latest version: %v", err)
+-			logging.Logger.Debug().Msgf("error saving cached latest version: %v", err)
 -		}
 -	}
 -
@@ -76,7 +89,134 @@
 -		LatestVersion: latestVersion,
 -		Cmd:           cmd,
 -	}, nil
+-}
+-
+-func skipUpdateCheck(ctx *config.RunContext) bool {
+-	return ctx.Config.SkipUpdateCheck || config.IsTest() || config.IsDev()
+-}
+-
+-func isBrewInstall() (bool, error) {
+-	if runtime.GOOS != "darwin" {
+-		return false, nil
+-	}
+-
+-	exe, err := os.Executable()
+-	if err != nil {
+-		return false, errors.Wrap(err, "error finding infracost executable")
+-	}
+-
+-	path, err := filepath.EvalSymlinks(exe)
+-	if err != nil {
+-		return false, errors.Wrap(err, "error evaluating infracost executable symlink")
+-	}
+-
+-	brewPrefixCmd := exec.Command("brew", "--prefix", "infracost")
+-
+-	var stdout bytes.Buffer
+-	brewPrefixCmd.Stdout = &stdout
+-
+-	err = brewPrefixCmd.Run()
+-	if err != nil {
+-		return false, errors.Wrap(err, "error running 'brew --prefix infracost'")
+-	}
+-
+-	brewPrefixPath, err := filepath.EvalSymlinks(strings.TrimSpace(stdout.String()))
+-	if err != nil {
+-		return false, errors.Wrap(err, "error evaluating brew prefix path symlink")
+-	}
+-
+-	brewInfracostPath := filepath.Join(brewPrefixPath, "bin", "infracost")
+-
+-	return path == brewInfracostPath, nil
+-}
+-
+-func getLatestBrewVersion() (string, error) {
+-	type versionsResp struct {
+-		Stable string `json:"stable"`
+-	}
+-
+-	type formulaResp struct {
+-		Versions versionsResp `json:"versions"`
+-	}
+-
+-	resp, err := http.Get("https://formulae.brew.sh/api/formula/infracost.json")
+-	if err != nil {
+-		return "", err
+-	}
+-
+-	if resp.StatusCode != http.StatusOK {
+-		return "", errors.Errorf("Error getting latest version: %s", resp.Status)
+-	}
+-
+-	body, err := io.ReadAll(resp.Body)
+-	if err != nil {
+-		return "", err
+-	}
+-	defer resp.Body.Close()
+-
+-	var parsedResp *formulaResp
+-	if json.Unmarshal(body, &parsedResp) != nil {
+-		return "", err
+-	}
+-
+-	v := parsedResp.Versions.Stable
+-	if !strings.HasPrefix(v, "v") {
+-		v = fmt.Sprintf("v%s", v)
+-	}
+-
+-	return v, nil
+-}
+-
+-func getLatestGitHubVersion() (string, error) {
+-	type releaseResp struct {
+-		TagName string `json:"tag_name"`
+-	}
+-
+-	resp, err := http.Get("https://api.github.com/repos/infracost/infracost/releases/latest")
+-	if err != nil {
+-		return "", err
+-	}
+-	defer resp.Body.Close()
+-
+-	body, err := io.ReadAll(resp.Body)
+-	if err != nil {
+-		return "", err
+-	}
+-
+-	var parsedResp *releaseResp
+-	if json.Unmarshal(body, &parsedResp) != nil {
+-		return "", err
+-	}
+-
+-	v := parsedResp.TagName
+-	if !strings.HasPrefix(v, "v") {
+-		v = fmt.Sprintf("v%s", v)
+-	}
+-
+-	return v, nil
+-}
+-
+-func checkCachedLatestVersion(ctx *config.RunContext) (string, error) {
+-	if ctx.State.LatestReleaseCheckedAt == "" {
+-		return "", nil
+-	}
+-
+-	checkedAt, err := time.Parse(time.RFC3339, ctx.State.LatestReleaseCheckedAt)
+-	if err != nil {
+-		return "", err
+-	}
+-
+-	if checkedAt.Before(time.Now().Add(-24 * time.Hour)) {
+-		return "", nil
+-	}
+-
+-	return ctx.State.LatestReleaseVersion, nil
+-}
+-
+-func setCachedLatestVersion(ctx *config.RunContext, latestVersion string) error {
+-	ctx.State.LatestReleaseVersion = latestVersion
+-	ctx.State.LatestReleaseCheckedAt = time.Now().Format(time.RFC3339)
+-
+-	return ctx.State.Save()
 +	return nil, nil
  }
- 
- func skipUpdateCheck(ctx *config.RunContext) bool {
