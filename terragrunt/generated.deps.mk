9c4314db95718807e312e2ac55d33ae9ccb5fccc
Oleksii Milevskyi <begemoth3663@gmail.com>
2025-02-01 21:37:45 +0200

    Upd terragrunt 0.72.5 -> 0.72.6

--- terragrunt/generated.deps.mk
+++ terragrunt/generated.deps.mk
@@ -1,7 +1,4 @@
 GH_TUPLE+=FiloSottile:age:v1.2.1:filosottile_age/vendor/filippo.io/age
-GH_TUPLE+=atomicgo:cursor:v0.2.0:atomicgo_cursor/vendor/atomicgo.dev/cursor
-GH_TUPLE+=atomicgo:keyboard:v0.2.9:atomicgo_keyboard/vendor/atomicgo.dev/keyboard
-GH_TUPLE+=atomicgo:schedule:v0.1.0:atomicgo_schedule/vendor/atomicgo.dev/schedule
 GH_TUPLE+=darccio:mergo:v1.0.1:darccio_mergo/vendor/dario.cat/mergo
 GH_TUPLE+=google:cel-spec:v0.19.1:google_cel_spec/vendor/cel.dev/expr
 GH_TUPLE+=open-telemetry:opentelemetry-go:v1.34.0:open_telemetry_opentelemetry_go/vendor/go.opentelemetry.io/otel
