38067c4a129dfe52352ef97da2e9223ff543b137
Oleksii Milevskyi <begemoth3663@gmail.com>
2025-03-11 16:50:23 +0200

    Add terraform 1.11.1

---
+++ terraform-1.11/generated.deps.mk
@@ -0,0 +1,4 @@
+GH_TUPLE+=kubernetes-sigs:json:f223a00ba0e2:kubernetes_sigs_json/vendor/sigs.k8s.io/json
+GH_TUPLE+=kubernetes-sigs:structured-merge-diff:v4.2.3:kubernetes_sigs_structured_merge_diff/vendor/sigs.k8s.io/structured-merge-diff/v4
+GH_TUPLE+=kubernetes:klog:v2.70.1:kubernetes_klog/vendor/k8s.io/klog/v2
+GH_TUPLE+=SSLMate:go-pkcs12:v0.4.0:sslmate_go_pkcs12/vendor/software.sslmate.com/src/go-pkcs12
