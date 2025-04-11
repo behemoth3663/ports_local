ffbbe6d925b9e1eca8b7d3e6ae18849c640d0182
Oleksii Milevskyi <begemoth3663@gmail.com>
2025-04-11 22:53:57 +0300

    Upd terragrunt 0.77.12 -> 0.77.13

--- terragrunt/generated.deps.mk
+++ terragrunt/generated.deps.mk
@@ -1,3 +1,4 @@
 GH_TUPLE+=FiloSottile:age:v1.2.1:filosottile_age/vendor/filippo.io/age
 GH_TUPLE+=darccio:mergo:v1.0.1:darccio_mergo/vendor/dario.cat/mergo
-GH_TUPLE+=google:cel-spec:v0.22.0:google_cel_spec/vendor/cel.dev/expr
+GH_TUPLE+=google:cel-spec:v0.22.1:google_cel_spec/vendor/cel.dev/expr
+GH_TUPLE+=FiloSottile:edwards25519:v1.1.0:filosottile_edwards25519/vendor/filippo.io/edwards25519
