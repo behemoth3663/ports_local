1523c355bb967cf72259f1a9264304bd037f8e28
Oleksii Milevskyi <begemoth3663@gmail.com>
2025-05-04 22:50:09 +0300

    terragrunt: 0.77.22 -> 0.78.0

--- terragrunt/files/patch-cli_app.go
+++ terragrunt/files/patch-cli_app.go
@@ -1,9 +1,18 @@
---- cli/app.go.orig	2025-03-28 15:11:40 UTC
+--- cli/app.go.orig	2025-05-02 16:13:41 UTC
 +++ cli/app.go
-@@ -97,22 +97,6 @@ func (app *App) RunContext(ctx context.Context, args [
+@@ -9,7 +9,6 @@ import (
+ 
+ 	"github.com/gruntwork-io/terragrunt/engine"
+ 	"github.com/gruntwork-io/terragrunt/internal/os/signal"
+-	"github.com/gruntwork-io/terragrunt/telemetry"
+ 	"golang.org/x/text/cases"
+ 	"golang.org/x/text/language"
+ 
+@@ -85,22 +84,6 @@ func (app *App) RunContext(ctx context.Context, args [
+ 	defer cancel()
  
  	ctx = app.registerGracefullyShutdown(ctx)
- 
+-
 -	if err := global.NewTelemetryFlags(app.opts, nil).Parse(os.Args); err != nil {
 -		return err
 -	}
@@ -19,7 +28,6 @@
 -	}(ctx)
 -
 -	ctx = telemetry.ContextWithTelemeter(ctx, telemeter)
--
+ 
  	ctx = config.WithConfigValues(ctx)
  	// configure engine context
- 	ctx = engine.WithEngineValues(ctx)
