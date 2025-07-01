261a29289b4ae5484279335609fe2fc572d4ddf2
Oleksii Milevskyi <begemoth3663@gmail.com>
2025-07-01 13:58:47 +0300

    golangci-lint: 2.1.6 -> 2.2.1

--- golangci-lint/generated.deps.mk
+++ golangci-lint/generated.deps.mk
@@ -3,3 +3,5 @@ GH_TUPLE+=go-simpler:sloglint:v0.11.0:go_simpler_sloglint/vendor/go-simpler.org/
 GH_TUPLE+=leighmcculloch:gocheckcompilerdirectives:v1.3.0:leighmcculloch_gocheckcompilerdirectives/vendor/4d63.com/gocheckcompilerdirectives
 GH_TUPLE+=leighmcculloch:gochecknoglobals:v0.2.2:leighmcculloch_gochecknoglobals/vendor/4d63.com/gochecknoglobals
 GH_TUPLE+=Crocmagnon:fatcontext:v0.8.0:crocmagnon_fatcontext/vendor/go.augendre.info/fatcontext
+GH_TUPLE+=Crocmagnon:arangolint:v0.2.0:crocmagnon_arangolint/vendor/go.augendre.info/arangolint
+GH_TUPLE+=chavacava:garif:v0.2.0:chavacava_garif/vendor/codeberg.org/chavacava/garif
