a13eec1ef1bccdefe90a7a34b91e46569acb3997
Oleksii Milevskyi <begemoth3663@gmail.com>
2025-07-09 22:55:15 +0300

    gh: 2.74.2 -> 2.75.0

--- gh/generated.deps.mk
+++ gh/generated.deps.mk
@@ -1,2 +1,1 @@
-GH_TUPLE+=imdario:mergo:v1.0.1:imdario_mergo/vendor/dario.cat/mergo
-GH_TUPLE+=kubernetes:klog:v2.130.1:kubernetes_klog/vendor/k8s.io/klog/v2
+GH_TUPLE+=imdario:mergo:v1.0.2:imdario_mergo/vendor/dario.cat/mergo
