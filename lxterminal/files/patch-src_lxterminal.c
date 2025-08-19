b22b99e34f872ffcc3867200cab0586906ef612c
Oleksii Milevskyi <begemoth3663@gmail.com>
2025-08-19 16:43:47 +0300

    lxterminal: 0.4.1_1 -> 0.4.1_2: Add Alt+Shift+1-Alt+Shift+9,Alt+Shift+0 accelerators for tabs 11-20

--- lxterminal/files/patch-src_lxterminal.c
+++ lxterminal/files/patch-src_lxterminal.c
@@ -8,21 +8,54 @@
  static void terminal_vte_size_allocate_event(GtkWidget *widget, GtkAllocation *allocation, Term *term);
  static void terminal_window_title_changed_event(GtkWidget * vte, Term * term);
  static gboolean terminal_close_window_confirmation_event(GtkWidget * widget, GdkEventButton * event, LXTerminal * terminal);
-@@ -290,11 +291,11 @@ static void terminal_initialize_switch_tab_accelerator
+@@ -290,24 +291,30 @@ static void terminal_initialize_switch_tab_accelerator
   * These switch to the tab selected by the digit, if it exists. */
  static void terminal_initialize_switch_tab_accelerator(Term * term)
  {
 -    if ((term->index + 1) < 10)
-+    if ((term->index + 1) <= 10)
-     {
-         /* Formulate the accelerator name. */
-         char switch_tab_accel[1 + 3 + 1 + 1 + 1]; /* "<ALT>n" */
+-    {
+-        /* Formulate the accelerator name. */
+-        char switch_tab_accel[1 + 3 + 1 + 1 + 1]; /* "<ALT>n" */
 -        sprintf(switch_tab_accel, "<ALT>%d", term->index + 1);
-+        sprintf(switch_tab_accel, "<ALT>%d", (term->index + 1) % 10);
++    guint key;
++    GdkModifierType mods = GDK_MOD1_MASK;
++    term->closure = g_cclosure_new_swap(G_CALLBACK(terminal_switch_tab_accelerator), term, NULL);
  
-         /* Parse the accelerator name. */
-         guint key;
-@@ -814,13 +815,38 @@ static void terminal_switch_page_event(GtkNotebook * n
+-        /* Parse the accelerator name. */
+-        guint key;
+-        GdkModifierType mods;
+-        gtk_accelerator_parse(switch_tab_accel, &key, &mods);
+-
+-        /* Define the accelerator. */
+-        term->closure = g_cclosure_new_swap(G_CALLBACK(terminal_switch_tab_accelerator), term, NULL);
+-        if (gtk_accel_group_from_accel_closure(term->closure) == NULL)
+-        {
+-            gtk_accel_group_connect(term->parent->accel_group, key, mods, GTK_ACCEL_LOCKED, term->closure);
+-        }
++    if (term->index < 10)
++    {
++        key = GDK_KEY_0 + (term->index + 1) % 10;
+     }
++    else if (term->index < 20)
++    {
++        mods |= GDK_SHIFT_MASK;
++        static const guint shifted_keys[] = {
++            GDK_KEY_exclam, GDK_KEY_at, GDK_KEY_numbersign, GDK_KEY_dollar,
++            GDK_KEY_percent, GDK_KEY_asciicircum, GDK_KEY_ampersand, GDK_KEY_asterisk,
++            GDK_KEY_parenleft, GDK_KEY_parenright
++        };
++        key = shifted_keys[term->index % 10];
++    }
++    else
++    {
++        return;
++    }
++
++    gtk_accel_group_connect(term->parent->accel_group, key, mods, GTK_ACCEL_LOCKED, term->closure);
+ }
+ 
+ /* update <ALT>n status. */
+@@ -814,13 +821,38 @@ static void terminal_switch_page_event(GtkNotebook * n
  
          /* Propagate the title to the toplevel window. */
          const gchar * title = gtk_label_get_text(GTK_LABEL(term->label));
@@ -62,7 +95,7 @@
  /* Handler for "window-title-changed" signal on a Term. */
  static void terminal_window_title_changed_event(GtkWidget * vte, Term * term)
  {
-@@ -1691,6 +1717,8 @@ LXTerminal * lxterminal_initialize(LXTermWindow * lxte
+@@ -1691,6 +1723,8 @@ LXTerminal * lxterminal_initialize(LXTermWindow * lxte
          G_CALLBACK(terminal_settings_apply), terminal);
      g_signal_connect(G_OBJECT(terminal->notebook), "switch-page", 
          G_CALLBACK(terminal_switch_page_event), terminal);
