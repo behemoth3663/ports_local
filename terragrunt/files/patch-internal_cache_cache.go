47fdbb756f83b988d9802b8d9352e69acc6e5984
Oleksii Milevskyi <begemoth3663@gmail.com>
2025-09-11 11:58:13 +0300

    terragrunt: 0.87.0 -> 0.87.1

--- terragrunt/files/patch-internal_cache_cache.go
+++ terragrunt/files/patch-internal_cache_cache.go
@@ -1,4 +1,4 @@
---- internal/cache/cache.go.orig	2025-03-28 15:11:40 UTC
+--- internal/cache/cache.go.orig	2025-09-10 19:24:04 UTC
 +++ internal/cache/cache.go
 @@ -9,8 +9,6 @@ import (
  	"fmt"
@@ -24,17 +24,18 @@
  	return value, found
  }
  
-@@ -53,7 +43,6 @@ func (c *Cache[V]) Put(ctx context.Context, key string
- func (c *Cache[V]) Put(ctx context.Context, key string, value V) {
+@@ -54,8 +44,6 @@ func (c *Cache[V]) Put(ctx context.Context, key string
  	c.Mutex.Lock()
  	defer c.Mutex.Unlock()
--	telemetry.TelemeterFromContext(ctx).Count(ctx, c.Name+"_cache_put", 1)
  
+-	telemetry.TelemeterFromContext(ctx).Count(ctx, c.Name+"_cache_put", 1)
+-
  	keyHash := sha256.Sum256([]byte(key))
  	cacheKey := hex.EncodeToString(keyHash[:])
-@@ -87,22 +76,17 @@ func (c *ExpiringCache[V]) Get(ctx context.Context, ke
- 	c.Mutex.Lock()
+ 	c.Cache[cacheKey] = value
+@@ -89,22 +77,17 @@ func (c *ExpiringCache[V]) Get(ctx context.Context, ke
  	defer c.Mutex.Unlock()
+ 
  	item, found := c.Cache[key]
 -	telemetry.TelemeterFromContext(ctx).Count(ctx, c.Name+"_cache_get", 1)
  
@@ -55,7 +56,7 @@
  	return item.Value, true
  }
  
-@@ -111,7 +95,6 @@ func (c *ExpiringCache[V]) Put(ctx context.Context, ke
+@@ -113,7 +96,6 @@ func (c *ExpiringCache[V]) Put(ctx context.Context, ke
  	c.Mutex.Lock()
  	defer c.Mutex.Unlock()
  
