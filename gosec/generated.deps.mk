771b8ec30136b3a32254f6b4d16ce4df4c4f58ee
Oleksii Milevskyi <begemoth3663@gmail.com>
2025-09-23 23:27:43 +0300

    gosec: 2.22.8 -> 2.22.9

---
+++ gosec/generated.deps.mk
@@ -0,0 +1,2 @@
+GH_TUPLE+=googleapis:google-cloud-go:v0.8.0:googleapis_google_cloud_go/vendor/cloud.google.com/go/compute/metadata
+GH_TUPLE+=googleapis:go-genai:v1.25.0:googleapis_go_genai/vendor/google.golang.org/genai
