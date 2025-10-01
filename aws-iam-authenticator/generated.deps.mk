e23e335c1c6539b8e1278b34aed54d7a6e3b2561
Oleksii Milevskyi <begemoth3663@gmail.com>
2025-10-01 16:38:40 +0300

    aws-iam-authenticator: 0.7.7 -> 0.7.8

--- aws-iam-authenticator/generated.deps.mk
+++ aws-iam-authenticator/generated.deps.mk
@@ -2,3 +2,4 @@ GH_TUPLE+=kubernetes:klog:v2.130.1:kubernetes_klog/vendor/k8s.io/klog/v2
 GH_TUPLE+=kubernetes-sigs:json:cfa47c3a1cc8:kubernetes_sigs_json/vendor/sigs.k8s.io/json
 GH_TUPLE+=kubernetes-sigs:structured-merge-diff:v6.3.0:kubernetes_sigs_structured_merge_diff/vendor/sigs.k8s.io/structured-merge-diff/v6
 GH_TUPLE+=kubernetes-sigs:randfill:v1.0.0:kubernetes_sigs_randfill/vendor/sigs.k8s.io/randfill
+GH_TUPLE+=go-yaml:yaml:v3.0.4:go_yaml_yaml/vendor/go.yaml.in/yaml/v3
