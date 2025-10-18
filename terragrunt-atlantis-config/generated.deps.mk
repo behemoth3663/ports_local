5de326235558221189fda6384047d4a9389a5c09
Oleksii Milevskyi <begemoth3663@gmail.com>
2025-10-18 10:45:44 +0300

    save

--- terragrunt-atlantis-config/generated.deps.mk
+++ terragrunt-atlantis-config/generated.deps.mk
@@ -1,2 +1,2 @@
-GH_TUPLE+=FiloSottile:age:v1.0.0:filosottile_age/vendor/filippo.io/age
-GH_TUPLE+=mozilla:sops:v3.7.3:mozilla_sops/vendor/go.mozilla.org/sops/v3
+GH_TUPLE+=FiloSottile:age:v1.2.1:filosottile_age/vendor/filippo.io/age
+GH_TUPLE+=googleapis:google-cloud-go:v0.6.0:googleapis_google_cloud_go/vendor/cloud.google.com/go/compute/metadata
