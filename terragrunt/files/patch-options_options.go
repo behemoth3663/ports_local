c26f307f53388b41a3969be9ab01048aa6f054c6
Oleksii Milevskyi <begemoth3663@gmail.com>
2025-12-16 17:07:37 +0200

    ssh-manager: 1.83.0 -> 1.84.0; terragrunt: 0.95.1 -> 0.96.0

--- terragrunt/files/patch-options_options.go
+++ terragrunt/files/patch-options_options.go
@@ -1,4 +1,4 @@
---- options/options.go.orig	2025-03-28 15:11:40 UTC
+--- options/options.go.orig	2025-12-15 17:07:12 UTC
 +++ options/options.go
 @@ -24,7 +24,6 @@ import (
  	"github.com/gruntwork-io/terragrunt/pkg/log"
@@ -8,7 +8,7 @@
  	"github.com/gruntwork-io/terragrunt/util"
  	"github.com/hashicorp/go-version"
  	"github.com/puzpuzpuz/xsync/v3"
-@@ -102,8 +101,6 @@ type TerragruntOptions struct {
+@@ -108,8 +107,6 @@ type TerragruntOptions struct {
  	FeatureFlags *xsync.MapOf[string, string] `clone:"shadowcopy"`
  	// Options to use engine for running IaC operations.
  	Engine *EngineOptions
@@ -17,11 +17,11 @@
  	// Attributes to override in AWS provider nested within modules as part of the aws-provider-patch command.
  	AwsProviderPatchOverrides map[string]string
  	// A command that can be used to run Terragrunt with the given options.
-@@ -399,7 +396,6 @@ func NewTerragruntOptionsWithWriters(stdout, stderr io
- 		ReadFiles:                  xsync.NewMapOf[string, []string](),
- 		StrictControls:             controls.New(),
- 		Experiments:                experiment.NewExperiments(),
--		Telemetry:                  new(telemetry.Options),
- 		NoStackValidate:            false,
- 		NoStackGenerate:            false,
- 	}
+@@ -433,7 +430,6 @@ func NewTerragruntOptionsWithWriters(stdout, stderr io
+ 		Errors:                           defaultErrorsConfig(),
+ 		StrictControls:                   controls.New(),
+ 		Experiments:                      experiment.NewExperiments(),
+-		Telemetry:                        new(telemetry.Options),
+ 		NoStackValidate:                  false,
+ 		NoStackGenerate:                  false,
+ 		VersionManagerFileName:           defaultVersionManagerFileName,
