fb6405af915bf1699d274348994bc027b209d846
Oleksii Milevskyi <begemoth3663@gmail.com>
2026-01-10 09:02:29 +0200

    cosign: 3.0.3 -> 3.0.4

--- cosign/generated.deps.mk
+++ cosign/generated.deps.mk
@@ -1,8 +1,8 @@
-GH_TUPLE+=kubernetes-sigs:json:cfa47c3a1cc8:kubernetes_sigs_json/vendor/sigs.k8s.io/json
+GH_TUPLE+=kubernetes-sigs:json:2d320260d730:kubernetes_sigs_json/vendor/sigs.k8s.io/json
 GH_TUPLE+=kubernetes-sigs:randfill:v1.0.0:kubernetes_sigs_randfill/vendor/sigs.k8s.io/randfill
 GH_TUPLE+=kubernetes-sigs:release-utils:v0.12.2:kubernetes_sigs_release_utils/vendor/sigs.k8s.io/release-utils
 GH_TUPLE+=kubernetes-sigs:structured-merge-diff:v6.3.0:kubernetes_sigs_structured_merge_diff/vendor/sigs.k8s.io/structured-merge-diff/v6
 GH_TUPLE+=kubernetes:klog:v2.130.1:kubernetes_klog/vendor/k8s.io/klog/v2
 GH_TUPLE+=go-yaml:yaml:v2.4.3:go_yaml_yaml/vendor/go.yaml.in/yaml/v2
-GH_TUPLE+=cue-lang:cue:v0.15.1:cue_lang_cue/vendor/cuelang.org/go
+GH_TUPLE+=cue-lang:cue:v0.15.3:cue_lang_cue/vendor/cuelang.org/go
 GH_TUPLE+=cue-labs:oci:074d06050084:cue_labs_oci/vendor/cuelabs.dev/go/oci/ociregistry
