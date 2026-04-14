75d176657b03a5f8591f416736d00d2f5bcf7917
Oleksii Milevskyi <begemoth3663@gmail.com>
2026-04-14 23:34:56 +0300

    govulncheck: 1.1.4 -> 1.2.0

--- govulncheck/files/patch-vendor_golang.org_x_tools_internal_goroot_importcfg.go
+++ govulncheck/files/patch-vendor_golang.org_x_tools_internal_goroot_importcfg.go
@@ -1,11 +1,11 @@
---- vendor/golang.org/x/tools/internal/goroot/importcfg.go.orig	2025-05-08 09:33:18 UTC
+--- vendor/golang.org/x/tools/internal/goroot/importcfg.go.orig	2026-04-09 21:00:41 UTC
 +++ vendor/golang.org/x/tools/internal/goroot/importcfg.go
-@@ -47,7 +47,7 @@ func PkgfileMap() (map[string]string, error) {
- func PkgfileMap() (map[string]string, error) {
- 	once.Do(func() {
- 		m := make(map[string]string)
--		output, err := exec.Command("go", "list", "-export", "-e", "-f", "{{.ImportPath}} {{.Export}}", "std", "cmd").Output()
-+		output, err := exec.Command("%%GO_CMD%%", "list", "-export", "-e", "-f", "{{.ImportPath}} {{.Export}}", "std", "cmd").Output()
- 		if err != nil {
- 			stdlibPkgfileErr = err
- 		}
+@@ -25,7 +25,7 @@ var pkgFileMapOnce = sync.OnceValues(func() (map[strin
+ 
+ var pkgFileMapOnce = sync.OnceValues(func() (map[string]string, error) {
+ 	m := make(map[string]string)
+-	output, err := exec.Command("go", "list", "-export", "-e", "-f", "{{.ImportPath}} {{.Export}}", "std", "cmd").Output()
++	output, err := exec.Command("%%GO_CMD%%", "list", "-export", "-e", "-f", "{{.ImportPath}} {{.Export}}", "std", "cmd").Output()
+ 	if err != nil {
+ 		return m, err
+ 	}
