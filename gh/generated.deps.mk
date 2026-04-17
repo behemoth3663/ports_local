96aad203204c53d37681ea61d30e1324f5fb9efb
Oleksii Milevskyi <begemoth3663@gmail.com>
2026-04-17 17:49:33 +0300

    gh: 2.89.0 -> 2.90.0

--- gh/generated.deps.mk
+++ gh/generated.deps.mk
@@ -1,5 +1,5 @@
 GH_TUPLE+=imdario:mergo:v1.0.2:imdario_mergo/vendor/dario.cat/mergo
-GH_TUPLE+=charmbracelet:bubbles:v2.0.0:charmbracelet_bubbles/vendor/charm.land/bubbles/v2
+GH_TUPLE+=charmbracelet:bubbles:v2.1.0:charmbracelet_bubbles/vendor/charm.land/bubbles/v2
 GH_TUPLE+=charmbracelet:bubbletea:v2.0.2:charmbracelet_bubbletea/vendor/charm.land/bubbletea/v2
 GH_TUPLE+=charmbracelet:lipgloss:v2.0.2:charmbracelet_lipgloss/vendor/charm.land/lipgloss/v2
 GH_TUPLE+=charmbracelet:huh:v2.0.3:charmbracelet_huh/vendor/charm.land/huh/v2
