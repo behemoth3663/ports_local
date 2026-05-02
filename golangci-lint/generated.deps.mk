7d090215b69f478afa5a0f5589af7e2221149043
Oleksii Milevskyi <begemoth3663@gmail.com>
2026-05-02 23:03:21 +0300

    golangci-lint: 2.11.4 -> 2.12.1

--- golangci-lint/generated.deps.mk
+++ golangci-lint/generated.deps.mk
@@ -1,10 +1,11 @@
 GH_TUPLE+=go-simpler:musttag:v0.14.0:go_simpler_musttag/vendor/go-simpler.org/musttag
-GH_TUPLE+=go-simpler:sloglint:v0.11.1:go_simpler_sloglint/vendor/go-simpler.org/sloglint
+GH_TUPLE+=go-simpler:sloglint:v0.12.0:go_simpler_sloglint/vendor/go-simpler.org/sloglint
+GH_TUPLE+=charmbracelet:lipgloss:v2.0.3:charmbracelet_lipgloss/vendor/charm.land/lipgloss/v2
+GH_TUPLE+=chavacava:garif:v0.2.0:chavacava_garif/vendor/codeberg.org/chavacava/garif
 GH_TUPLE+=leighmcculloch:gocheckcompilerdirectives:v1.3.0:leighmcculloch_gocheckcompilerdirectives/vendor/4d63.com/gocheckcompilerdirectives
 GH_TUPLE+=leighmcculloch:gochecknoglobals:v0.2.2:leighmcculloch_gochecknoglobals/vendor/4d63.com/gochecknoglobals
+GH_TUPLE+=polyfloyd:go-errorlint:v1.9.0:polyfloyd_go_errorlint/vendor/codeberg.org/polyfloyd/go-errorlint
 GH_TUPLE+=Crocmagnon:fatcontext:v0.9.0:crocmagnon_fatcontext/vendor/go.augendre.info/fatcontext
 GH_TUPLE+=Crocmagnon:arangolint:v0.4.0:crocmagnon_arangolint/vendor/go.augendre.info/arangolint
-GH_TUPLE+=chavacava:garif:v0.2.0:chavacava_garif/vendor/codeberg.org/chavacava/garif
-GH_TUPLE+=polyfloyd:go-errorlint:v1.9.0:polyfloyd_go_errorlint/vendor/codeberg.org/polyfloyd/go-errorlint
 GH_TUPLE+=GaijinEntertainment:golib:v0.6.0:gaijinentertainment_golib/vendor/dev.gaijin.team/go/golib
 GH_TUPLE+=GaijinEntertainment:go-exhaustruct:v4.0.0:gaijinentertainment_go_exhaustruct/vendor/dev.gaijin.team/go/exhaustruct/v4
