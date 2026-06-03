0c69b1a6bc5664801038f26cdc8e279136449a65
Oleksii Milevskyi <begemoth3663@gmail.com>
2026-06-03 15:52:49 +0300

    gosec: 2.26.1 -> 2.27.1

--- gosec/files/patch-vendor_cloud.google.com_go_auth_grpctransport_grpctransport.go
+++ gosec/files/patch-vendor_cloud.google.com_go_auth_grpctransport_grpctransport.go
@@ -1,36 +1,116 @@
---- vendor/cloud.google.com/go/auth/grpctransport/grpctransport.go.orig	2025-01-27 22:44:05 UTC
+--- vendor/cloud.google.com/go/auth/grpctransport/grpctransport.go.orig	2026-03-23 17:47:48 UTC
 +++ vendor/cloud.google.com/go/auth/grpctransport/grpctransport.go
-@@ -31,7 +31,6 @@ import (
- 	"cloud.google.com/go/auth/internal"
- 	"cloud.google.com/go/auth/internal/transport"
+@@ -34,9 +34,6 @@ import (
+ 	"github.com/googleapis/gax-go/v2"
+ 	"github.com/googleapis/gax-go/v2/callctx"
  	"github.com/googleapis/gax-go/v2/internallog"
 -	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+-	"go.opentelemetry.io/otel/attribute"
+-	"go.opentelemetry.io/otel/trace"
  	"google.golang.org/grpc"
+ 	"google.golang.org/grpc/codes"
  	grpccreds "google.golang.org/grpc/credentials"
- 	grpcinsecure "google.golang.org/grpc/credentials/insecure"
-@@ -65,15 +64,6 @@ var (
- 	otelStatsHandler         stats.Handler
- )
- 
--// otelGRPCStatsHandler returns singleton otelStatsHandler for reuse across all
--// dial connections.
--func otelGRPCStatsHandler() stats.Handler {
--	initOtelStatsHandlerOnce.Do(func() {
--		otelStatsHandler = otelgrpc.NewClientHandler()
--	})
--	return otelStatsHandler
--}
--
- // ClientCertProvider is a function that returns a TLS client certificate to be
- // used when opening TLS connections. It follows the same semantics as
- // [crypto/tls.Config.GetClientCertificate].
-@@ -431,8 +421,5 @@ func addOpenTelemetryStatsHandler(dialOpts []grpc.Dial
+@@ -459,22 +456,7 @@ func addOpenTelemetryStatsHandler(dialOpts []grpc.Dial
  }
  
  func addOpenTelemetryStatsHandler(dialOpts []grpc.DialOption, opts *Options) []grpc.DialOption {
 -	if opts.DisableTelemetry {
--		return dialOpts
+ 		return dialOpts
 -	}
--	return append(dialOpts, grpc.WithStatsHandler(otelGRPCStatsHandler()))
-+	return dialOpts
+-	if !gax.IsFeatureEnabled("TRACING") {
+-		return append(dialOpts, grpc.WithStatsHandler(otelgrpc.NewClientHandler()))
+-	}
+-	var staticAttrs []attribute.KeyValue
+-	if opts.InternalOptions != nil {
+-		staticAttrs = transport.StaticTelemetryAttributes(opts.InternalOptions.TelemetryAttributes)
+-	}
+-	otelOpts := []otelgrpc.Option{
+-		otelgrpc.WithSpanAttributes(staticAttrs...),
+-	}
+-	return append(dialOpts, grpc.WithStatsHandler(&otelHandler{
+-		Handler: otelgrpc.NewClientHandler(otelOpts...),
+-	}))
  }
+ 
+ // otelHandler is a wrapper around the OpenTelemetry gRPC client handler that
+@@ -487,80 +469,12 @@ func (h *otelHandler) TagRPC(ctx context.Context, info
+ // name and retry count from the outgoing context metadata and attach them to
+ // the current span.
+ func (h *otelHandler) TagRPC(ctx context.Context, info *stats.RPCTagInfo) context.Context {
+-	ctx = h.Handler.TagRPC(ctx, info)
+-	span := trace.SpanFromContext(ctx)
+-	if !span.IsRecording() {
+-		return ctx
+-	}
+-	var attrs []attribute.KeyValue
+-	if resName, ok := callctx.TelemetryFromContext(ctx, "resource_name"); ok {
+-		attrs = append(attrs, attribute.String("gcp.resource.destination.id", resName))
+-	}
+-	if resendCountStr, ok := callctx.TelemetryFromContext(ctx, "resend_count"); ok {
+-		if count, err := strconv.Atoi(resendCountStr); err == nil {
+-			attrs = append(attrs, attribute.Int("gcp.grpc.resend_count", count))
+-		}
+-	}
+-	if len(attrs) > 0 {
+-		span.SetAttributes(attrs...)
+-	}
+ 	return ctx
+ }
+ 
+ // HandleRPC intercepts the RPC completion to capture and format error-related
+ // attributes ensuring they conform to Google Cloud observability standards.
+ func (h *otelHandler) HandleRPC(ctx context.Context, s stats.RPCStats) {
+-	end, ok := s.(*stats.End)
+-	if !ok {
+-		h.Handler.HandleRPC(ctx, s)
+-		return
+-	}
+-	span := trace.SpanFromContext(ctx)
+-	if !span.IsRecording() {
+-		h.Handler.HandleRPC(ctx, s)
+-		return
+-	}
+-
+-	var attrs []attribute.KeyValue
+-	if end.Error != nil {
+-		st, ok := status.FromError(end.Error)
+-		rpcStatusCode := codeToCanonicalStr(st.Code())
+-
+-		var errorType string
+-		// 1. Check if the local context expired or was cancelled. This is the only
+-		// reliable way to distinguish a local client timeout from a server timeout
+-		// because gRPC does not wrap context errors in its status.Error types.
+-		if errors.Is(ctx.Err(), context.DeadlineExceeded) {
+-			errorType = "CLIENT_TIMEOUT"
+-		} else if errors.Is(ctx.Err(), context.Canceled) {
+-			errorType = "CLIENT_CANCELLED"
+-		} else if !ok || st.Code() == codes.Unknown || st.Code() == codes.Internal {
+-			// 2. If the error isn't a context breakdown and the gRPC framework
+-			// doesn't "understand" it (returning ok=false or a generic catch-all
+-			// bucket like Unknown/Internal), we "pack" the actual Go error type
+-			// name into error.type (e.g., "*net.OpError"). This is per the error.type
+-			// [spec](https://opentelemetry.io/docs/specs/semconv/registry/attributes/error/#error-type).
+-			// "When error.type is set to a type (e.g., an exception type), its canonical
+-			// class name identifying the type within the artifact SHOULD be used."
+-			errorType = fmt.Sprintf("%T", end.Error)
+-		} else {
+-			// 3. Otherwise, it is a well-understood gRPC protocol error (e.g.,
+-			// PERMISSION_DENIED) likely returned by the server.
+-			errorType = rpcStatusCode
+-		}
+-
+-		attrs = []attribute.KeyValue{
+-			attribute.String("error.type", errorType),
+-			attribute.String("status.message", st.Message()),
+-			attribute.String("rpc.response.status_code", rpcStatusCode),
+-			attribute.String("exception.type", fmt.Sprintf("%T", end.Error)),
+-		}
+-	} else {
+-		attrs = []attribute.KeyValue{
+-			attribute.String("rpc.response.status_code", "OK"),
+-		}
+-	}
+-	span.SetAttributes(attrs...)
+ 	h.Handler.HandleRPC(ctx, s)
+ }
+ 
--- gosec/files/patch-vendor_cloud.google.com_go_auth_httptransport_transport.go
+++ gosec/files/patch-vendor_cloud.google.com_go_auth_httptransport_transport.go
@@ -1,22 +1,106 @@
---- vendor/cloud.google.com/go/auth/httptransport/transport.go.orig	2025-01-27 22:44:05 UTC
+--- vendor/cloud.google.com/go/auth/httptransport/transport.go.orig	2026-03-23 17:47:48 UTC
 +++ vendor/cloud.google.com/go/auth/httptransport/transport.go
-@@ -27,7 +27,6 @@ import (
- 	"cloud.google.com/go/auth/internal"
+@@ -17,12 +17,9 @@ import (
+ import (
+ 	"context"
+ 	"crypto/tls"
+-	"errors"
+-	"fmt"
+ 	"net"
+ 	"net/http"
+ 	"os"
+-	"strconv"
+ 	"time"
+ 
+ 	"cloud.google.com/go/auth"
+@@ -31,11 +28,6 @@ import (
  	"cloud.google.com/go/auth/internal/transport"
  	"cloud.google.com/go/auth/internal/transport/cert"
+ 	"cloud.google.com/go/auth/internal/transport/headers"
+-	"github.com/googleapis/gax-go/v2"
+-	"github.com/googleapis/gax-go/v2/callctx"
 -	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+-	"go.opentelemetry.io/otel/attribute"
+-	"go.opentelemetry.io/otel/trace"
  	"golang.org/x/net/http2"
  )
  
-@@ -169,10 +168,7 @@ func addOpenTelemetryTransport(trans http.RoundTripper
+@@ -177,22 +169,7 @@ func addOpenTelemetryTransport(trans http.RoundTripper
  }
  
  func addOpenTelemetryTransport(trans http.RoundTripper, opts *Options) http.RoundTripper {
 -	if opts.DisableTelemetry {
--		return trans
+ 		return trans
 -	}
--	return otelhttp.NewTransport(trans)
-+	return trans
+-	if !gax.IsFeatureEnabled("TRACING") {
+-		return otelhttp.NewTransport(trans)
+-	}
+-	var staticAttrs []attribute.KeyValue
+-	if opts.InternalOptions != nil {
+-		staticAttrs = transport.StaticTelemetryAttributes(opts.InternalOptions.TelemetryAttributes)
+-	}
+-	otelOpts := []otelhttp.Option{
+-		otelhttp.WithSpanOptions(trace.WithAttributes(staticAttrs...)),
+-	}
+-	return otelhttp.NewTransport(&otelAttributeTransport{
+-		base: trans,
+-	}, otelOpts...)
  }
  
- type authTransport struct {
+ // otelAttributeTransport is a wrapper around an http.RoundTripper that adds
+@@ -205,55 +182,7 @@ func (t *otelAttributeTransport) RoundTrip(req *http.R
+ // OpenTelemetry span with static and dynamic attributes, as well as detailed
+ // error information.
+ func (t *otelAttributeTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+-	span := trace.SpanFromContext(req.Context())
+-	if span.IsRecording() {
+-		var attrs []attribute.KeyValue
+-		attrs = append(attrs, attribute.String("rpc.system.name", "http"))
+-		if resName, ok := callctx.TelemetryFromContext(req.Context(), "resource_name"); ok {
+-			attrs = append(attrs, attribute.String("gcp.resource.destination.id", resName))
+-		}
+-		if resendCountStr, ok := callctx.TelemetryFromContext(req.Context(), "resend_count"); ok {
+-			if count, err := strconv.Atoi(resendCountStr); err == nil {
+-				attrs = append(attrs, attribute.Int("http.request.resend_count", count))
+-			}
+-		}
+-		if urlTemplate, ok := callctx.TelemetryFromContext(req.Context(), "url_template"); ok {
+-			attrs = append(attrs, attribute.String("url.template", urlTemplate))
+-			span.SetName(fmt.Sprintf("%s %s", req.Method, urlTemplate))
+-		}
+-		span.SetAttributes(attrs...)
+-	}
+-
+ 	resp, err := t.base.RoundTrip(req)
+-
+-	if span.IsRecording() {
+-		if err != nil {
+-			var errorType string
+-			switch {
+-			case errors.Is(err, context.DeadlineExceeded):
+-				errorType = "CLIENT_TIMEOUT"
+-			case errors.Is(err, context.Canceled):
+-				errorType = "CLIENT_CANCELLED"
+-			default:
+-				errorType = "CLIENT_CONNECTION_ERROR"
+-			}
+-			span.SetAttributes(
+-				attribute.String("error.type", errorType),
+-				attribute.String("status.message", err.Error()),
+-				attribute.String("exception.type", fmt.Sprintf("%T", err)),
+-			)
+-		} else {
+-			span.SetAttributes(attribute.Int("http.response.status_code", resp.StatusCode))
+-			if resp.StatusCode >= 400 {
+-				errorType := strconv.Itoa(resp.StatusCode)
+-				span.SetAttributes(
+-					attribute.String("error.type", errorType),
+-					attribute.String("status.message", resp.Status),
+-				)
+-			}
+-		}
+-	}
+-
+ 	return resp, err
+ }
+ 
